a651177cf606f829307c516c6ca01c2653825ebd
Anand Javaji <anandjavaji@Andy-Mac-3.local>
2018-07-04 13:41:56 +1000

    First commit demo spring boot Rest API

---
+++ src/main/java/com/experiment/spring/demo/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.experiment.spring.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.experiment.spring.demo.dto.Employee;
+
+
+@RestController
+@RequestMapping("employees")
+ public class EmployeeController {
+
+    Employee employee = new Employee();
+
+    @RequestMapping(value="/{name}", method = RequestMethod.GET, produces = "application/json")
+    public Employee getEmployee(@PathVariable String name) {
+        employee.setName("test");
+        employee.setEmployeeId("emp123");
+        return employee;
+   }
+}
---
+++ src/main/java/com/experiment/spring/demo/dto/Employee.java
@@ -0,0 +1,25 @@
+package com.experiment.spring.demo.dto;
+
+public class Employee {
+
+    private String name;
+
+    private String employeeId;
+
+    public String getName() {
+        return name;
+
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+}
